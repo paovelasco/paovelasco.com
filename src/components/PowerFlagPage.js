70b72825340a2ee2bd8117a9d163e8d95ce47f03
Pvelasco <Pvelasco@vailresorts.com>
2017-12-20 16:41:12 -0700

    modifying minor styles

--- src/components/PowerFlagPage.js
+++ src/components/PowerFlagPage.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const PowerFlagPage = () => {
   return (
-    <div>
-      <h1>Ready Player 1...</h1>
-      <h2>YOUR GAME BEGINS NOW</h2>
+    <div className="powerflag-content">
+      <div className="main-content">
+        <h3>READY PLAYER 1...</h3>
+        <h2>YOUR GAME BEGINS NOW</h2>
+      </div>
+      <ul>
+        <li><a href="https://www.facebook.com/goalcast/videos/1578895815520936/" target="_blank">Ready player one. Your game begins now</a></li>
+        <li><a href="" target="_blank">If you want to change the world. Start by making your bed</a></li>
+      </ul>
     </div>
   );
 };
