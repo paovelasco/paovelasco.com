5a68e34c102922be9a980cff427eee11ced584a8
Pvelasco <Pvelasco@vailresorts.com>
2018-01-03 15:09:46 -0700

    removing sample

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Pablo Velasco</h1>
       <h2>Software Engineer</h2>
-      <ul>
-        <li>Review the <Link to="/fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ul>
     </div>
   );
 };
