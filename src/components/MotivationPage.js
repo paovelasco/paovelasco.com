850ffcfe93b8fff1fd7d421bcd5ff2efef8ea54b
Pvelasco <Pvelasco@vailresorts.com>
2018-01-04 16:03:09 -0700

    adding motivational videos

--- src/components/MotivationPage.js
+++ src/components/MotivationPage.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const MotivationPage = () => {
   return (
-    <div>
+    <div className="motivation-content">
+      <h2>MOTIVATION</h2>
       <ul>
-        <li><a href="https://www.facebook.com/goalcast/videos/1578895815520936/" target="_blank">Ready player one. Your game begins now</a></li>
-        <li><a href="" target="_blank">If you want to change the world. Start by making your bed</a></li>
+        <li>Steve Jobs - <a href="https://www.facebook.com/GymBlogCentral/videos/1150137638365410/" target="_blank">Your time is limited so don't waste it living someone else's life</a></li>
+        <li>Matt Damon - <a href="https://www.facebook.com/goalcast/videos/1578895815520936/" target="_blank">Ready player one. Your game begins now</a></li>
+        <li>Former US Navy Admiral, William H. McRaven - <a href="https://www.facebook.com/goalcast/videos/1533350906742094/" target="_blank">If you want to change the world. Start by making your bed</a></li>
       </ul>
     </div>
   );
