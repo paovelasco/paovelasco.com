5b3fe4ecfb08887aea250452ab3f2571006a63df
Pvelasco <Pvelasco@vailresorts.com>
2018-01-05 08:55:52 -0700

    Adding data file and more motivational links

--- src/components/MotivationPage.js
+++ src/components/MotivationPage.js
@@ -8,6 +8,8 @@ const MotivationPage = () => {
         <li>Steve Jobs - <a href="https://www.facebook.com/GymBlogCentral/videos/1150137638365410/" target="_blank">Your time is limited so don't waste it living someone else's life</a></li>
         <li>Matt Damon - <a href="https://www.facebook.com/goalcast/videos/1578895815520936/" target="_blank">Ready player one. Your game begins now</a></li>
         <li>Former US Navy Admiral, William H. McRaven - <a href="https://www.facebook.com/goalcast/videos/1533350906742094/" target="_blank">If you want to change the world. Start by making your bed</a></li>
+        <li>Motivation Grid - <a href="https://www.youtube.com/watch?v=wzhzkKccBi8&list=PL2075269699E6695E&index=19" target="_blank">Manageable steps to make that dream a reality</a></li>
+        <li>Tony Robbins - <a href="https://www.youtube.com/watch?v=mgmVOuLgFB0&list=PL2075269699E6695E&index=24" target="_blank">Pain is temporary. If I quit it will last forever</a></li>
       </ul>
     </div>
   );
