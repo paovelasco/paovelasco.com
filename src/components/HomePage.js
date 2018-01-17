b4087cd1356160af041a314edf2639de47fbf112
Pvelasco <Pvelasco@vailresorts.com>
2018-01-17 15:29:56 -0700

    linkedin link in home page added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import linkedInImage from '../../assests/images/linkedIn.png';
+import {Link} from 'react-router-dom';
 
 const HomePage = () => {
   return (
@@ -6,6 +8,7 @@ const HomePage = () => {
       <div className="header">
         <h1>Pablo Velasco</h1>
         <h2>Software Engineer</h2>
+        <Link to='https://www.linkedin.com/in/pablo-velasco-455731a/' target='_blank'><img src={linkedInImage} /></Link>
       </div>
     </div>
   );
